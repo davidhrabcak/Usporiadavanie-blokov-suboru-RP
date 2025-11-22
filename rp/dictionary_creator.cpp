9daf9066c7f89ef6e56313719093cbc20eaa333f
davidhrabcak <davidko.hrabcak@gmail.com>
2025-11-22 22:40:01 +0100

    add frequency calculation, remembering list of most likely subsequent words, saved to txt file

--- rp/dictionary_creator.cpp
+++ rp/dictionary_creator.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 #include <sstream>
 #include <vector>
 #include <string>
 #include <unordered_map>
 using namespace std;
+
+#define MAX_TOPLIST 10 // limits number of most frequent words for every first word
+
+// first argument when launching program determines output file, default dictionary_custom.txt
 
 vector<string> split_text(string sen) {
     stringstream ss(sen);
@@ -17,8 +22,7 @@ vector<string> split_text(string sen) {
     
     return words;
 }
-//TODO markov chain frequency analysis - use unordered_map<string, vector<string>>,
-// where first element in vector is the most frequent successor
+
 int main(int argc, char const *argv[]) {
     vector<string> sources;
     ifstream file("input_books.txt"); // input filename
@@ -27,7 +31,39 @@ int main(int argc, char const *argv[]) {
     string data = buffer.str();
     vector<string> words = split_text(data);
 
-    unordered_map<string, vector<string>> dictionary;
+    unordered_map<string, unordered_map<string, int>> freq;
     for (int i = 0; i < words.size() - 1; i++) {
+        freq[words[i]][words[i+1]]++;
     }
+
+    unordered_map<string, vector<string>> dict;
+    priority_queue<pair<int, string>> pq;
+
+    for (auto& kv : freq) {
+        for (auto& entry : kv.second) {
+            pq.push(make_pair(entry.second, entry.first));
+        }
+
+        vector<string> top;
+        while (!pq.empty() && top.size() < MAX_TOPLIST) {
+            top.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        dict[kv.first] = top;
+    }
+    string filename = "dictionary_custom.txt";
+    if (argc >= 2) {
+        filename = argv[1];
+    }
+    ofstream out(filename);
+    for (auto& kv : dict) {
+        out << kv.first;
+        for (string w : kv.second) {
+            out << " " << w;
+        }
+        out << '\n';
+    }
+
+    return 0;
 }
