659d36ab582e647c9b951ca9639a6d9b4293f6a9
davidhrabcak <davidko.hrabcak@gmail.com>
2025-11-26 01:03:31 +0100

    clean up segment.cpp

--- rp/segment.cpp
+++ rp/segment.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <string>
 #include <syscall.h>
-#include <string.h>
 using namespace std;
+
 // 1st argument: input file
 // 2nd argument: output file
 // 3rd argument: segment size
@@ -11,15 +11,9 @@ int main(int argc, char const *argv[]) {
     // split -b [arg_3] -d [arg_1] segment_
     // ls segment_* | shuf | xargs -I {} sh -c 'cat {}; echo' > [arg_2]
     string command = "split -b ";
-    string part1 = " -d ";
-    string part2 = " segment_";
     string command2 = "ls segment_* | shuf | xargs -I {} sh -c 'cat {}; echo' >";
 
-    command.append(argv[3]);
-    command.append(part1);
-    command.append(argv[1]);
-    command.append(part2);
-
+    command.append(argv[3]).append(" -d ").append(argv[1]).append(" segment_");
     command2.append(argv[2]);
 
     system(command.c_str());
