c44511f6632dca757a3b168dbc3ffd3d058cf146
davidhrabcak <davidko.hrabcak@gmail.com>
2026-03-09 19:52:09 +0100

    add frame data to frame scanner with proper printing

--- rp/sound/frame_scanner.cpp
+++ rp/sound/frame_scanner.cpp
@@ -6,10 +6,30 @@
 
 using namespace std;
 
+struct FrameData {
+    string mpeg_version;
+    string layer;
+    bool crc;
+    int bitrate;
+    int sample_rate;
+    string channel_mode;
+};
+
 struct FrameInfo {
     size_t position;   // byte offset in file
     int length;     // frame length in bytes
+    FrameData frame_data;
 };
+
+ostream& operator<<(ostream &os, const FrameData& data) {
+    os << "MPEG: " << data.mpeg_version << endl;
+    os << "Layer: " << data.layer << endl;
+    os << "CRC: " << ((data.crc) ? "Used" : "Not used") << endl;
+    os << "Bitrate: " << data.bitrate << endl;
+    os << "Sample rate: " << data.sample_rate << endl;
+    os << "Channel mode: " << data.channel_mode << endl;
+    return os;
+}
 
 class Mp3FrameScanner {
 public:
@@ -25,9 +45,14 @@ public:
         return frames.size();
     }
 
+    FrameData getFrame(const size_t index) {
+        return frame_data.at(index);
+    }
+
 private:
     vector<uint8_t> data;
     vector<FrameInfo> frames;
+    vector<FrameData> frame_data;
 
     void loadFile(const string& filename) {
         ifstream file(filename, ios::binary);
@@ -48,7 +73,7 @@ private:
         if (data.size() < 10) return 0;
 
         if (data[0] == 'I' && data[1] == 'D' && data[2] == '3') {
-            size_t size =
+            const size_t size =
                 ((data[6] & 0x7F) << 21) |
                 ((data[7] & 0x7F) << 14) |
                 ((data[8] & 0x7F) << 7) |
@@ -95,6 +120,9 @@ private:
             }
 
             frames.push_back({i, frameLength});
+            frame_data.push_back({header.getMpegVersion(), header.getLayer(),
+                                        header.hasCRC(), header.getBitrate(),
+                                        header.getSampleRate(), header.getChannelMode()});
 
             i += frameLength; // jump to next frame
         }
@@ -104,6 +132,7 @@ private:
 int main(int argc, char** argv) {
     auto s = Mp3FrameScanner("../../sound/file_example_MP3_700KB.mp3");
     cout << s.getFrames().data()->length << endl;
+    cout << s.getFrame(1000);
     cout << s.getFrameCount() << endl;
 
 }
