6eddac55d5f642071aa83a00559ec2cbac2f4e4f
davidhrabcak <davidko.hrabcak@gmail.com>
2026-06-18 01:47:33 +0200

    uncomment commented code, fix CMakeLists.txt

--- rp/sound/test/bindings.cpp
+++ rp/sound/test/bindings.cpp
@@ -1,14 +1,14 @@
-// #include <pybind11/pybind11.h>
-// #include <pybind11/stl.h>       // needed for automatic vector conversion
-// #include "../frame_scanner.hpp"
-//
-// namespace py = pybind11;
-//
-// PYBIND11_MODULE(frame_scanner, m) {
-//     py::class_<FrameData>(m, "FrameData")
-//         .def_readonly("rawBits", &FrameData::rawBits);
-//
-//     py::class_<Mp3FrameScanner>(m, "Mp3FrameScanner")
-//         .def(py::init<const std::string &>())
-//         .def_readonly("frame_data", &Mp3FrameScanner::frame_data);
-// }
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>       // needed for automatic vector conversion
+#include "../frame_scanner.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(frame_scanner, m) {
+    py::class_<FrameData>(m, "FrameData")
+        .def_readonly("rawBits", &FrameData::rawBits);
+
+    py::class_<Mp3FrameScanner>(m, "Mp3FrameScanner")
+        .def(py::init<const std::string &>())
+        .def_readonly("frame_data", &Mp3FrameScanner::frame_data);
+}
